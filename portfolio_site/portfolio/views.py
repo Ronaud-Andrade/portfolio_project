6838bb04f07d6401b9d11749603d1e9ffe0e1b6f
Chapadin <ronaudandrade108@gmail.com>
2025-09-08 00:01:06 -0300

    Update

--- portfolio_site/portfolio/views.py
+++ portfolio_site/portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+#from .models import Projetos
 # Create your views here.
+
+def home(request):
+    context = {
+        'nome': 'Ronaud',
+        'titulo': 'Meu portfolio',
+    }
+
+    return render(request, 'portfolio/home.html', context)
