798e2ff39136a511b3bf71e3eb2b02c4d6d0e993
Chapadin <ronaudandrade108@gmail.com>
2025-09-08 16:48:16 -0300

    Testando os templates

--- portfolio_site/portfolio/views.py
+++ portfolio_site/portfolio/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import Projetos_novos
 #from .models import Projetos
 # Create your views here.
 
 def home(request):
+    projetos_ronaud = Projetos_novos.objects.filter(dono='Ronaud')
+    projetos_iago = Projetos_novos.objects.filter(dono='Iago')
+
     context = {
-        'nome': 'Ronaud',
-        'titulo': 'Meu portfolio',
+        'projetos_ronaud' : projetos_ronaud, 'projetos_iago' : projetos_iago
     }
 
     return render(request, 'portfolio/home.html', context)
