3a1535aa23b3a06cb35bb09368b285b4420b5dfb
Chapadin <ronaudandrade108@gmail.com>
2025-09-13 10:41:32 -0300

    Criação da página index

--- portfolio_site/portfolio/urls.py
+++ portfolio_site/portfolio/urls.py
@@ -5,6 +5,7 @@ app_name = 'portfolio'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index/', views.index, name='index')
     #path('about/', views.about, name='about'),
     #path('projects/', views.projects, name='projects'),
     #path('contact/', views.contact, name='contact'),
--- portfolio_site/portfolio/views.py
+++ portfolio_site/portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Projetos_novos
+from .models import Habili_contatos
 #from .models import Projetos
 # Create your views here.
 
@@ -7,8 +8,29 @@ def home(request):
     projetos_ronaud = Projetos_novos.objects.filter(dono='Ronaud')
     projetos_iago = Projetos_novos.objects.filter(dono='Iago')
 
+    habili_ronaud = Habili_contatos.objects.filter(pessoa = 'Ronaud').first()
+
+
     context = {
-        'projetos_ronaud' : projetos_ronaud, 'projetos_iago' : projetos_iago
+        'projetos_ronaud' : projetos_ronaud, 
+        'projetos_iago' : projetos_iago, 
+        'habili_ronaud' :  habili_ronaud, 
     }
 
     return render(request, 'portfolio/home.html', context)
+
+
+def index(request):
+    projetos_ronaud = Projetos_novos.objects.filter(dono='Ronaud')
+    projetos_iago = Projetos_novos.objects.filter(dono='Iago')
+
+    habili_ronaud = Habili_contatos.objects.filter(pessoa = 'Ronaud').first()
+
+
+    context = {
+        'projetos_ronaud' : projetos_ronaud, 
+        'projetos_iago' : projetos_iago, 
+        'habili_ronaud' :  habili_ronaud, 
+    }
+
+    return render(request, 'portfolio/index.html', context)
